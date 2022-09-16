5fc650839740754f5f7c881dd21873afe0865aab
= <ryaninternet62@gmail.com>
2022-09-15 21:09:54 -0300

    Add box with sublime.

--- src/main/java/br/com/overlord/gui/game/GamePanel.java
+++ src/main/java/br/com/overlord/gui/game/GamePanel.java
@@ -1,6 +1,10 @@
 package br.com.overlord.gui.game;
 
 import br.com.overlord.gui.component.Panel;
+import br.com.overlord.window.WindowUtil;
+
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class GamePanel extends Panel {
 	private static final long serialVersionUID = -2165762423550088479L;
@@ -8,5 +12,16 @@ public class GamePanel extends Panel {
 	
 	public GamePanel() {
 		super();
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int renderX = (WindowUtil.getDimension().width - 200) / 2 ;
+		int renderY = (WindowUtil.getDimension().height - 50) / 2;
+		
+		g.fillRect(renderX, renderY, 200, 50);
+		g.setColor(Color.decode("#ff0000"));
+		g.drawOval(renderX, (renderY + 50), 200, 0);
 	}
 }
